5e3a04d38b76b54f6ffbe3637bd1b22b4f888971
Lakshya Singh <lakshay.singh1108@gmail.com>
2022-09-16 19:17:12 +0530

    feat: astral rays
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ fn main() {
 struct Line {
     points: Vec<Vec2>,
     length: f32,
+    color: Srgba
 }
 
 impl Line {
@@ -18,6 +19,7 @@ impl Line {
         Self {
             points: vec![p],
             length: 0.0,
+            color: srgba(random_f32(), random_f32(), random_f32(), random_f32())
         }
     }
 }
@@ -32,8 +34,6 @@ const SCREEN_HEIGHT: usize = 1024;
 const MAX_LENGTH: f32 = SCREEN_WIDTH as f32 * 2.0;
 const LINES: usize = (SCREEN_HEIGHT * SCREEN_WIDTH) / 1000;
 const SN: f32 = 0.001;
-const COLOR: f32 = 200.0;
-const STEP: f32 = SCREEN_WIDTH as f32 * 0.01;
 
 fn model(app: &App) -> Model {
     let _window = app
@@ -65,14 +65,13 @@ fn update(_app: &App, model: &mut Model, _update: Update) {
             continue;
         }
 
+        let d_change = model.noise.get([(last.x * SN) as f64, (last.y * SN) as f64, 0.0]) as f32 * PI;
 
-        let d_change = model.noise.get([(last.x * SN) as f64, (last.y * SN) as f64]) as f32 * PI;
-
-        let map_d_change = map_range(d_change, -1.0, 1.0, -PI, PI);
+        let map_d_change = map_range(d_change, -1.0, 1.0, -360.0, 360.0);
 
         let new = *last + vec2(
-            map_d_change.cos() * STEP,
-            map_d_change.sin() * STEP
+            map_d_change.cos(),
+            map_d_change.sin()
         );
 
         line.length +=
@@ -85,26 +84,18 @@ fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
 
     if app.elapsed_frames() == 1 {
-        draw.background().color(srgba(0.0, 0.0, 0.0, 0.8));
+        draw.background().color(BLACK);
     }
     // draw.rect().w_h(SCREEN_WIDTH as f32, SCREEN_HEIGHT as f32).color(srgba(0.0, 0.0, 0.0, 0.1));
 
     for line in model.lines.iter() {
-        let mut line_length = 0.0;
-        let items = line.points.len();
-        for idx in 0..=items - 2 {
-            let (current, next) = (line.points[idx], line.points[idx + 1]);
-
-            line_length += f32::sqrt(
-                (current.x - next.x) * (current.x - next.x)
-                    + (current.y - next.y) * (current.y - next.y),
-            );
-
-            let sat = 3.0 * (line.length - line_length) / line.length;
-            let hue = (COLOR + 130.0 * (SCREEN_HEIGHT as f32 - current.y) / SCREEN_HEIGHT as f32) % 360.0 / 360.0;
-
-            draw.line().points(current, next).hsva(hue, sat, 1.0, 0.2).weight(2.0);
+        if line.length > MAX_LENGTH {
+            continue;
         }
+        draw.polyline()
+            .weight(3.0)
+            .points(line.points.iter().cloned())
+            .color(line.color);
     }
 
     draw.to_frame(app, &frame).unwrap();
